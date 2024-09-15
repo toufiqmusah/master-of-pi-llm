0a6c7fd35ddd1bd79aa59cad204593204b310d8e
toufiqmsah <toufiqmusah32@gmail.com>
2024-09-15 03:14:20 +0100

    test run, images

--- app.py
+++ app.py
@@ -3,13 +3,13 @@ import streamlit as st
 
 model = 'qwen2:0.5b-instruct'
 
-st.title('My pi LLM')
+st.title('pi LLM')
 st.logo('images/raspberry-pi.svg')
 
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
-# Display chat messages from history on app rerun
+# Display chat messages from history
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
@@ -34,51 +34,8 @@ if prompt:
         )
 
         for response_chunk in response_stream:
-            # Assuming each response_chunk is a dict with a 'content' key
             token = response_chunk['message']['content']
             full_response += token
-            # Update the assistant's message in the chat
             message_placeholder.markdown(full_response)
 
-        # Add the assistant's response to the session state
         st.session_state.messages.append({"role": "assistant", "content": full_response})
-
-
-'''import ollama
-import streamlit as st
-
-model = 'qwen2:0.5b-instruct'
-
-st.title('My pi LLM')
-st.logo('images/raspberry-pi.svg')
-
-prompt = st.chat_input("Enter Your Prompt Here")
-
-response_stream = ollama.chat(model = model, 
-							  messages = [
-								{
-									'role':'user',
-									'content':prompt,
-								}],)
-
-if "messages" not in st.session_state:
-    st.session_state.messages = []
-
-# Display chat messages from history on app rerun
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
-
-
-if prompt:
-    with st.chat_message("user"):
-        st.markdown(prompt)
-    st.session_state.messages.append({"role": "user", "content": prompt})
-
-response = response_stream['message']['content']
-
-with st.chat_message("assistant"):
-    st.markdown(response) 
-
-st.session_state.messages.append({"role": "assistant", "content": response})
-'''
